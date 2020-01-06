f355c83533f8e39ab0344ba4be780251b30ee165
nicky <962943478@qq.com>
2020-01-06 18:15:57 +0800

    left-nav

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,19 +1,88 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
+import { Redirect, Route } from 'react-router-dom'
+import { Layout } from 'antd'
 import memoryUtils from '../../utils/memoryUtils'
-import { Redirect } from 'react-router-dom'
-
+import Header from '../../components/header'
+import LeftNav from '../../components/left-nav'
+import Home from '../../pages/home/home'
+const { Footer, Sider, Content } = Layout
 
 export default class Admin extends Component {
-    render(){
+    render() {
         let user = memoryUtils.user
-        
-        if(!user || !user._id){
+
+        if (!user || !user._id) {
             // 自动跳转render中
-            return <Redirect to="/login"/>   //render中跳转
+            return <Redirect to="/login" />   //render中跳转
         }
         return (
-            
-            <div>hello {user.username}</div>
+            <Layout style={{ height: '100%' }}>
+                <Sider>
+                    <LeftNav />
+                </Sider>
+                <Layout>
+                    <Header>Header</Header>
+                    <Content style={{ backgroundColor: 'white' }}>
+                        Content,{user.username}
+                        {/* <Route
+
+                            path='/home'
+
+                            component={Home} />
+
+                        <Route
+
+                            path='/category'
+
+                            component={Category} />
+
+                        <Route
+
+                            path='/product'
+
+                            component={Product} />
+
+                        <Route
+
+                            path='/role'
+
+                            component={Role} />
+
+                        <Route
+
+                            path='/user'
+
+                            component={User} />
+
+                        <Route
+
+                            path='/charts/bar'
+
+                            component={Bar} />
+
+                        <Route
+
+                            path='/charts/line'
+
+                            component={Line} />
+
+                        <Route
+
+                            path='/charts/pie'
+
+                            component={Pie} />
+
+                        <Redirect
+
+                            to='/home'
+
+                        /> */}
+                    </Content>
+                    <Footer
+                        style={{ textAlign: 'center', color: '#aaaaaa' }}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
+
+                </Layout>
+            </Layout>
         )
     }
 }
